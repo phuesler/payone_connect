02e27f8d92c8585ea072b15d4308a3d30ca5ac12
Alexander Lang <alex@upstre.am>
2013-12-10 17:07:13 +0100

    converts specs to use `expect` instead of `should`

--- spec/payone_connect_spec.rb
+++ spec/payone_connect_spec.rb
@@ -26,21 +26,23 @@ describe PayoneConnect do
   end
 
   describe "connection" do
-    it "should connect to the payone gateway" do
+    it "connects to the payone gateway" do
       https_connection = Net::HTTP.new(@uri.host, @uri.port)
-      Net::HTTP.should_receive(:new).with(@uri.host,@uri.port).and_return(https_connection)
+
+      expect(Net::HTTP).to receive(:new).with(@uri.host,@uri.port) { https_connection }
+
       @finance_gate_connection.request
     end
 
-    it "should set the x form header" do
-      @finance_gate_connection.request_header.should == {'Content-Type'=> 'application/x-www-form-urlencoded'}
+    it "sets the x form header" do
+      expect(@finance_gate_connection.request_header).to eql('Content-Type'=> 'application/x-www-form-urlencoded')
     end
   end
 
   describe "general parameter assignment" do
     %w(portalid mid mode key encoding request).each do |parameter|
-      it "should set the #{parameter} parameter" do
-        @finance_gate_connection.request_data.should include("#{parameter}=#{@request_data[parameter]}")
+      it "sets the #{parameter} parameter" do
+        expect(@finance_gate_connection.request_data).to include("#{parameter}=#{@request_data[parameter]}")
       end
     end
   end
@@ -50,16 +52,16 @@ describe PayoneConnect do
       FakeWeb.register_uri(:post, "https://testapi.pay1.de/post-gateway/", :body => "status=APPROVED\ntxid=18059493\nuserid=4923401")
     end
 
-    it "should return the status" do
-      @finance_gate_connection.request[:status].should == "APPROVED"
+    it "returns the status" do
+      expect(@finance_gate_connection.request[:status]).to eql("APPROVED")
     end
 
-    it "should return payones transaction id" do
-      @finance_gate_connection.request[:txid].should == "18059493"
+    it "returns payone's transaction id" do
+      expect(@finance_gate_connection.request[:txid]).to eql("18059493")
     end
 
-    it "should return payones user id" do
-      @finance_gate_connection.request[:userid].should == "4923401"
+    it "returns payone's user id" do
+      expect(@finance_gate_connection.request[:userid]).to eql("4923401")
     end
 
     it 'ignores empty lines' do
@@ -70,24 +72,25 @@ describe PayoneConnect do
   end
 
   describe "response is redirect" do
-    it "should parse the redirect url correctly" do
+    it "parses the redirect url correctly" do
       FakeWeb.register_uri(:post, "https://testapi.pay1.de/post-gateway/", :body => "status=REDIRECT\ntxid=18059493\nuserid=4923401\nredirecturl=https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=ABC&useraction=commit")
-      @finance_gate_connection.request[:redirecturl].should == "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=ABC&useraction=commit"
+
+      expect(@finance_gate_connection.request[:redirecturl]).to eql("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=ABC&useraction=commit")
     end
   end
 
   describe "authorization parameter assignment" do
     %w(aid reference currency amount clearingtype).each do |parameter|
-      it "should set the #{parameter} parameter" do
-        @finance_gate_connection.request_data.should include("#{parameter}=#{@request_data[parameter]}")
+      it "sets the #{parameter} parameter" do
+        expect(@finance_gate_connection.request_data).to include("#{parameter}=#{@request_data[parameter]}")
       end
     end
   end
 
   describe "credit card payment parameter assignment" do
     %w(cardpan cardtype cardexpiredate cardcvc2 cardholder).each do |parameter|
-      it "should set the #{parameter} parameter" do
-        @finance_gate_connection.request_data.should include("#{parameter}=#{URI.encode(@request_data[parameter].to_s)}")
+      it "sets the #{parameter} parameter" do
+        expect(@finance_gate_connection.request_data).to include("#{parameter}=#{URI.encode(@request_data[parameter].to_s)}")
       end
     end
   end
